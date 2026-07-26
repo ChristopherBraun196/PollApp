646114adac1db4ccd05d8ef0eeb54377fcdb2d6b
ChristopherBraun196 <232182424+ChristopherBraun196@users.noreply.github.com>
2026-07-26 11:59:48 +0200

    Implement main app structure with header, hero, and surveys components

--- Poll-App/src/app/app.ts
+++ Poll-App/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Hero } from './components/hero/hero';
+import { Surveys } from "./components/surveys/surveys";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Hero, Surveys],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
