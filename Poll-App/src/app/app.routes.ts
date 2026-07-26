12d3c6113e831796dadcbdc45e6e9d3b23d11dd8
ChristopherBraun196 <232182424+ChristopherBraun196@users.noreply.github.com>
2026-07-26 12:55:42 +0200

    Add home, new survey, and survey detail components; update routing and modify hero and surveys components for improved navigation

--- Poll-App/src/app/app.routes.ts
+++ Poll-App/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { NewSurvey } from './components/new-survey/new-survey';
+import { SurveyDetail } from './components/survey-detail/survey-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'new-survey', component: NewSurvey },
+  { path: 'survey/:id', component: SurveyDetail },
+];
