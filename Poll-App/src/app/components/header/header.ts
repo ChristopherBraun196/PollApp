c4c5a42231521ad65b1a26c2c2ddde45cd92f19d
ChristopherBraun196 <232182424+ChristopherBraun196@users.noreply.github.com>
2026-07-27 17:49:14 +0200

    Implement survey detail component with dynamic survey form and styling; add dialog for creating new surveys

--- Poll-App/src/app/components/header/header.ts
+++ Poll-App/src/app/components/header/header.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  private router = inject(Router);
+  private currentUrl = signal(this.router.url);
+
+  isSurveyDetail = computed(() => this.currentUrl().startsWith('/survey/'));
+
+  constructor() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentUrl.set(event.urlAfterRedirects);
+      }
+    });
+  }
+}
--- Poll-App/src/app/components/survey-detail/survey-detail.ts
+++ Poll-App/src/app/components/survey-detail/survey-detail.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, viewChild } from '@angular/core';
+import { NewSurvey } from '../new-survey/new-survey';
 
 @Component({
   selector: 'app-survey-detail',
-  imports: [],
+  imports: [NewSurvey],
   templateUrl: './survey-detail.html',
   styleUrl: './survey-detail.scss',
 })
-export class SurveyDetail {}
+export class SurveyDetail {
+  newSurveyDialog = viewChild(NewSurvey);
+  
+  openDialog() {
+    this.newSurveyDialog()?.open();
+  }
+}
